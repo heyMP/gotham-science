e66571152d24363253a4f13f0c0b41ba23732558
heyMP <mgp140@gmail.com>
2019-02-02 05:34:18 -0500

    converted monorepo to wcf factory

---
+++ elements/gotham-science-header/src/gotham-science-header.js
@@ -0,0 +1,52 @@
+/**
+ * Copyright 2019 Gotham University
+ * @license Apache-2.0, see License.md for full text.
+ */
+import { LitElement, html } from '@polymer/lit-element';
+import from "@gotham/gotham-logo"
+
+/**
+ * `gotham-science-header`
+ * ``
+ *
+ * @microcopy - language worth noting:
+ *  -
+ *
+ * @customElement
+ * @lit-html
+ * @lit-element
+ * @demo demo/index.html
+ */
+class GothamScienceHeader extends LitElement {
+  /* REQUIRED FOR TOOLING DO NOT TOUCH */
+
+  /**
+   * Store the tag name to make it easier to obtain directly.
+   * @notice function name must be here for tooling to operate correctly
+   */
+  tag() {
+    return "gotham-science-header";
+  }
+
+  // life cycle
+  constructor() {
+    super();
+
+  }
+  /**
+   * life cycle, element is afixed to the DOM
+   */
+  connectedCallback() {
+    super.connectedCallback();
+
+  }
+  // static get observedAttributes() {
+  //   return [];
+  // }
+  // disconnectedCallback() {}
+
+  // attributeChangedCallback(attr, oldValue, newValue) {}
+
+}
+customElements.define("gotham-science-header", GothamScienceHeader);
+export { GothamScienceHeader };
